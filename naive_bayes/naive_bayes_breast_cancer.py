3f8eddc131fb7d72574c7c383802f9898966d65d
Gabriel Tamujo Meyrer <gabrieltamujo@outlook.com>
2020-06-09 13:03:38 -0300

    feat: naive bayes breast cancer dataset

---
+++ naive_bayes/naive_bayes_breast_cancer.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+base = pd.read_csv("breast-cancer-wisconsin.csv")
+
+base.drop(index=base.loc[base['1.3'] == '?'].index, inplace=True)
+
+previsores = base.iloc[:, 1:9].values
+classe = base.iloc[:, 10].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+#One Hot Encoding
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [1, 2, 3, 4, 5, 6, 7])])
+previsores = column_tranformer.fit_transform(previsores).toarray()
+
+#Escalonamento
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+
+#Aplicação do Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+classificador = GaussianNB()
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+#Análise de Precisão
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
